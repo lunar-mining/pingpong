ed26a6bb03823215c981612aaa9c4027f2eb2c13
rachel-rose <ihateface@autistici.org>
2020-11-10 17:51:56 +0100

    got pong working

--- src/tcp-client.rs
+++ src/tcp-client.rs
@@ -1,22 +1,19 @@
 //! cargo run --example tcp-server
 //! cargo run --example tcp-client
 
-//1. Send a `Ping` message every 5 secs.
-
-//1. Start a timer
-//    1. When any new message is received, reset the timer to 0
-//    2. Once the timer reaches 10 secs then drop the connection
-
-//use smol::io::AsyncWriteExt;
-
-//use std::io::BufReader;
-use futures::AsyncWriteExt;
-//use async_dup::Arc;
-//use std::io::{Read, Write};
+//use smol::Timer;
+//use std::time::Duration;
 use std::net::TcpStream;
-//use async_std::net::TcpStream;
+use futures::AsyncWriteExt;
 use smol::{io, Async};
-//use async_std::io::ReadExt;
+use async_std::io::ReadExt;
+use std::str;
+
+//async fn sleep(dur: Duration) {
+//    Timer::after(dur).await;
+//}
+// TODO: start a smol task in parallel at start
+// task loops and every 5 seconds prints hello
 
 fn main() -> io::Result<()> {
     smol::block_on(async {
@@ -25,9 +22,12 @@ fn main() -> io::Result<()> {
         println!("Connected to {}", stream.get_ref().peer_addr()?);
 
         stream.write_all(b"Ping").await?;
-        println!("Wrote Ping to stream");
+        //println!("Wrote Ping to stream");
 
+        let mut buffer = [0u8; 4];
+        stream.read_exact(&mut buffer).await?;
+        let buffer = str::from_utf8(&buffer).unwrap();
+        println!("{}", buffer);
         Ok(())
     })
 }
-
